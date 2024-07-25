9870c475da0461091aace7d31c85931257eb2e1c
soldierforus <travis@tdbennett.com>
2024-07-25 07:00:20 -0700

    slider buttons & marana video

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,11 +2,12 @@ var slider = document.getElementById('slider')
 var sliderItem = slider.getElementsByTagName('div');
 var dots = document.getElementById('dots');
 var dotsChild = document.getElementById('dots').getElementsByTagName('li');
+
 for (i = 0; i < sliderItem.length; i++) {
   dots.appendChild(document.createElement('li'));
   dotsChild[i].classList.add('list-inline-item');
   dotsChild[i].setAttribute("id", i);
-  dotsChild[i].innerHTML = i;
+  dotsChild[i].innerHTML = i+1;
   dotsChild[0].classList.add('active');
   dotsChild[i].addEventListener("click", runSlider);
 }
